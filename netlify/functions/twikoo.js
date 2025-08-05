614a75c90ea55aec267ef5ef2b49115b1a9d5ab5
HHYYYY <hhyyyy@hhysky.eu.org>
2025-08-05 17:03:05 +0800

    Update twikoo.js

--- netlify/functions/twikoo.js
+++ netlify/functions/twikoo.js
@@ -1,1 +1,13 @@
 exports.handler = require('twikoo-netlify').handler
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': 'https://hhyyyy.cn',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type'
+    },
+    body: JSON.stringify({ message: 'Twikoo 云函数运行正常' })
+  };
+};
+
